c77fe119eb76aab080e99cffa58b049efb8bda4d
Munch310 <ghkdlsi@naver.com>
2023-09-26 17:40:25 +0900

    [ADD] RandomSpawn Tower
    - 버튼 클릭 시, 랜덤한 위치에 타워 스폰

--- Assets/Scripts/Tower/Draggable.cs
+++ Assets/Scripts/Tower/Draggable.cs
@@ -1,50 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class Draggable : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    [SerializeField] public int from;
-
-    private RectTransform rectTransform;
-    private CanvasGroup canvasGroup;
-    private Vector3 originPositions;
     public void OnBeginDrag(PointerEventData eventData)
     {
-        //Debug.Log("OnBeginDrag");
-        canvasGroup.blocksRaycasts = false;
-        canvasGroup.alpha = 0.7f;
-
+        throw new System.NotImplementedException();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        rectTransform.anchoredPosition += eventData.delta;
-        //Debug.Log("OnDrag");
+        throw new System.NotImplementedException();
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        //Debug.Log("OnEndDrag");
-        rectTransform.anchoredPosition = originPositions;
-        canvasGroup.blocksRaycasts = true;
-        canvasGroup.alpha = 1.0f;
+        throw new System.NotImplementedException();
     }
 
-    public void SetOriginalPosition(Vector3 newPositions)
+    public void OnPointerDown(PointerEventData eventData)
     {
-        originPositions = newPositions;
-    }
-
-    void Start()
-    {
-        rectTransform = GetComponent<RectTransform>();
-        canvasGroup = GetComponent<CanvasGroup>();
-        originPositions = rectTransform.anchoredPosition;
-    }
-    void Update()
-    {
-        
+        throw new System.NotImplementedException();
     }
 }
