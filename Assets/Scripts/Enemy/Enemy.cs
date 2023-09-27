f038affa433102522dc2693e4cc477eca4ab9e1b
xjwsx <134588087+xjwsx@users.noreply.github.com>
2023-09-27 11:45:01 +0900

    [UPDATE] Enemy Move
    적 움직임 기능 수정

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    ObjectPoolingManager manager = ObjectPoolingManager.instance;
+    EnemySpawner enemySpawner;
+    private Rigidbody2D rigidbody;
+    private Movement movement;
+    private int hp;
+    private int damage;
+    public bool isSqawn = false;
+    private Transform[] wayPoints;
+    private int currentIndex = 0;
+    int wayPointNum = 0;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        enemySpawner = GetComponent<EnemySpawner>();
+        movement = GetComponent<Movement>();
+    }
+    public void SetPosition(Transform[] wayPoints)
+    {
+        if (wayPoints == null || wayPoints.Length == 0)
+            return;
+        wayPointNum = wayPoints.Length;
+        this.wayPoints = new Transform[wayPointNum];
+        this.wayPoints = wayPoints;
+        currentIndex = 0;
+        transform.position = wayPoints[currentIndex].position;
+        StartCoroutine("MovePath");
+    }
+    IEnumerator MovePath()
+    {
+        NextMoveTo();
+
+        while(true)
+        {
+            if (Vector3.Distance(transform.position, wayPoints[currentIndex].position) < 0.02f * movement.MoveSpeed) 
+            {
+                NextMoveTo();
+            }
+            yield return null;
+        }
+        
+    }
+
+    private void NextMoveTo()
+    {
+        if(currentIndex < wayPointNum - 1)
+        {
+            transform.position = wayPoints[currentIndex].position;
+            currentIndex++;
+            Vector3 direction = (wayPoints[currentIndex].position - transform.position).normalized;
+            movement.MoveTo(direction);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        hp -= damage;
+        if(hp < 0 || collision.gameObject.CompareTag("end"))
+        {
+            wayPointNum = 0;
+            this.gameObject.SetActive(false);
+            isSqawn = false;
+        }
+    }
+}
--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -7,6 +7,8 @@ public class EnemySpawner : MonoBehaviour
 {
     private float sqawnTime;
     ObjectPoolingManager manager;
+    [SerializeField]
+    public Transform[] wayPos;
 
     private void Start()
     {
@@ -27,8 +29,8 @@ public class EnemySpawner : MonoBehaviour
     {
         thisEnemy.SetActive(true);
         Enemy param = thisEnemy.GetComponent<Enemy>();
-        param.SetPosition();
-        param.Sqawned();
+        param.SetPosition(wayPos);
+        //param.Sqawned();
     }
     private float NextSpawnTime()
     {
